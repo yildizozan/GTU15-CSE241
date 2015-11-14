c3fc4c0cd254720ed45412a8828f6fc82778d0a5
Ozan Yıldız <social@yildizozan.com>
2015-11-14 18:58:35 +0200

    Fundamentals of Main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ozan
+ *
+ * Created on November 12, 2015, 4:06 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
