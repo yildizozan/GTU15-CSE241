698d3ffe164c63a7d179d9179ce4bd8e223d52cc
Ozan Yıldız <social@yildizozan.com>
2015-11-28 04:30:56 +0200

    new Fundamental

--- Cell.cpp
+++ Cell.cpp
@@ -2,46 +2,26 @@
 * File:   Cell.cpp
 * Author: Ozan YILDIZ
 *
-* Created on November 12, 2015, 4:06 PM
+* Created on November 25, 2015, 4:06 PM
 */
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
 #include "Cell.h"
 
 Cell::Cell()
-	: axisX(NULL), axisY(" "), who(NULL)
 {
-
+    who = '2';
 }
 
-Cell::Cell(const Cell& copyCell)
-{
-	axisX = copyCell.axisX;
-	axisY = copyCell.axisY;
-	who = copyCell.who;
-}
-
-Cell::Cell(char newWho)
-	: who(newWho)
-{
-
-}
-
-
-Cell::Cell(int newAxisX, string newAxisY, char newWho)
+Cell::Cell(const int newAxisX, const string newAxisY, const char newWho)
 	: axisX(newAxisX), axisY(newAxisY), who(newWho)
 {
-
 }
 
-bool Cell::operator==(const Cell & temp)
+Cell::Cell(const Cell& orig)
 {
-	if (axisX == temp.axisX)
-		if (axisY == temp.axisY)
-			return true;
-
-	return false;
 }
+
 
