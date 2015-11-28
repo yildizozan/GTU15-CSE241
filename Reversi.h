a35c89b6510290434035221bb65a352e2b3f22cf
Ozan Yıldız <social@yildizozan.com>
2015-11-28 05:54:06 +0200

    new Fundamental2

--- Reversi.h
+++ Reversi.h
@@ -1,70 +1,50 @@
 /*
-* File:   Reversi.h
+* File:   Cell.cpp
 * Author: Ozan YILDIZ
 *
-* Created on November 12, 2015, 4:06 PM
+* Created on November 25, 2015, 4:06 PM
 */
 
 #ifndef REVERSI_H
-#define	REVERSI_H
+#define REVERSI_H
+
+#include <vector>
+#include <string>
+#include "Cell.h"
 
 using namespace std;
 
 class Reversi {
 public:
-	Reversi();
-	Reversi(const int, const int);
-
-	// Mutator & Accessor
-	int getRow() { return row; }
-	void setRow(const int newRow) { row = newRow; }
-
-	int getColumn() { return column; }
-	void setColumn(const int newColumn) { column = newColumn; }
-
-	int getWho() const { return who; }
-	void setWho(const int newWho) { who = newWho; }
-
-	bool getGameOver() const { return gameOver; }
-	void setGameOver(const int newGameOver) { who = newGameOver; }
-
-	int getPlayerScore() const { return playerScore; }
-	void setPlayerScore(const int newPlayerScore) { playerScore = newPlayerScore; }
-
-	int getAIScore() const { return AIScore; }
-	void setAIScore(const int newAIScore) { AIScore = newAIScore; }
-
-	// Input & Output Function
-	void input(int, string);
-	void output(void);
-
-	// Play game functions
-	void play();
-	void play(const int, const string);
-
-	// Other
-	void score();
-	void newValue();
-
-	// Operator Overloading
-
+    Reversi();
+    Reversi(const Reversi& orig);
+    
+    // Play Functions
+    void playGame();
+    void playGame(const int, const string);
+    
+    // Output Function
+    void output() const;
+    
+    // Ancestor & Mutators Functions
+    int getRow() const { return row; }
+    void setRow(const int temp) { row = temp; }
+    
+    int getColumn() const { return column; }
+    void setColumn(const int temp) { column = temp; }
+    
+    char getWho() const { return who; }
+    void setWho(const char newWho) { who = newWho; }
+    
 private:
-	int row, column;	// Row and Column
-	int who;			// Siranin kimde oldugunu tutuyor
-	int playerScore, AIScore;
-
-	vector< vector<Cell> > gameCell;
-
-	// Other
-	const bool gameOver();		// It check game for game over!
-	void expand(const int, const int);
-	void refresh();
-	void adding(const int, const string);
-	void control(const int, const string);
-	void calculate(const int, const int, const char);
-	const int findBestPosition(int, int);	// degiskenler degisecek const olmamali
-
-	
+    vector<vector<Cell> > gameCell;
+    char who;
+    
+    int row, column;
+    
+    bool control(const int x, const int y, const char who);
+    void newValue();
+    void find(const int x, const string y);
 };
 
-#endif	/* REVERSI_H */
+#endif /* REVERSI_H */
